edb642a24aa44d0b69c9be62bc0a50642ef05c02
Jerome Pratt <jerome.pratt@outlook.com>
2014-09-30 20:04:51 +0100

    Added error handling for email or username that's already been used

--- server.rb
+++ server.rb
@@ -47,7 +47,7 @@ post '/users' do
 		session[:user_id] = @user.id
 		redirect to ('/')
 	else
-		flash[:notice] = "Sorry, your passwords don't match"
+		flash.now[:errors] = @user.errors.full_messages
 		haml :"users/new"
 	end
 end
